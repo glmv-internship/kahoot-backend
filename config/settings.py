f121d87b9bf2ec461ebf7ece3fa17737c99b1de8
Mirzosharif Habibov <mirzoupwork@gmail.com>
2024-08-17 18:53:14 +0500

    changes

--- config/settings.py
+++ config/settings.py
@@ -116,7 +116,7 @@ DATABASES = {
     }
 }
 # Configure the database
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
--- core/serializers.py
+++ core/serializers.py
@@ -33,7 +33,7 @@ class QuizSerializer(serializers.ModelSerializer):
 class GameSerializer(serializers.ModelSerializer):
     host_uid = serializers.CharField(write_only=True)
     host = UserSerializer(read_only=True)
-    quiz = QuizSerializer(read_only=True)
+    # quiz = QuizSerializer(read_only=True)
     players = UserSerializer(many=True,read_only=True)
     class Meta:
         model = Game
@@ -51,7 +51,10 @@ class GameSerializer(serializers.ModelSerializer):
         game = Game.objects.create(host=host,**validated_data)
         return game
     def to_representation(self, instance):
-        return super().to_representation(instance)
+        
+        data = super().to_representation(instance)
+        data['quiz'] = QuizSerializer(Quiz.objects.get(id=instance.quiz.id)).data
+        return data
     
 class GameDetailSerializer(serializers.ModelSerializer):
     host = UserSerializer(read_only=True)
@@ -75,11 +78,19 @@ class GameDetailSerializer(serializers.ModelSerializer):
         return super().to_representation(instance)
     
 class UserResultSerializer(serializers.ModelSerializer):   
-    user = UserSerializer(read_only=True)
+    # user = UserSerializer(read_only=True)
     # game = GameSerializer(read_only=True)
     class Meta:
         model = UserResult
         fields = '__all__'
+        
+    def to_representation(self, instance):
+        # instance.game = GameSerializer(Game.objects.get(id=instance.game.id)).instance
+        # print(instance.game)
+        data = super().to_representation(instance)
+        print(data)
+        data['game'] = GameSerializer(Game.objects.get(id=instance.game.id)).data
+        return data
     
     
     
