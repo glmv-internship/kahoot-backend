aa01b16d8ba1b61e855be0614cb099bcf89db56b
Mirzosharif Habibov <mirzoupwork@gmail.com>
2024-08-19 21:03:40 +0500

    changes

--- config/settings.py
+++ config/settings.py
@@ -125,6 +125,7 @@ DATABASES = {
 }
 # Configure the database
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
